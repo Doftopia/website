998d83b200adb18f26d58e1d4d7e1f8ea002b57a
ogb4n <ogb4n@protonmail.com>
2024-03-06 00:40:02 +0100

    [💫] fetch name from api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,37 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Home: React.FC = () => {
+  const [itemName, setItemName] = useState<string>("");
+
+  useEffect(() => {
+    async function fetchItemData() {
+      try {
+        const responseItem = await axios.get(
+          "https://api.dofusdb.fr/items?id=2469"
+        );
+        setItemName(responseItem.data.data[0].name.fr);
+        const responseRecipe = await axios.get(
+          `https://api.dofusdb.fr/recipes/${responseItem.data.data[0].id}`
+        );
+      } catch (error) {}
+    }
+
+    fetchItemData();
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-6xl font-bold">Welcome to Doftopia</h1>
-    </main>
+    <div>
+      <h1>
+        Nom de l{"'"}objet : {itemName}
+      </h1>
+      <div>
+        <h2>Recette de l{"'"}objet :</h2>
+        <ul></ul>
+      </div>
+    </div>
   );
 };
 
