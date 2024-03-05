6f531a7bd670602d0af34116f00f7eb78f683b75
ogb4n <ogb4n@protonmail.com>
2024-03-05 22:26:34 +0100

    [⚒️ ] correction du patern de page | [💫] ajout composant navbar

---
+++ src/app/components/Navbar/navbar.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+interface NavbarProps {}
+
+const Navbar: React.FC<NavbarProps> = () => {
+  return (
+    <div>
+      <h1>Navbar</h1>
+    </div>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-export default function Home() {
+import React from "react";
+
+const Home: React.FC = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-6xl font-bold">Welcome to Doftopia</h1>
     </main>
   );
-}
+};
+
+export default Home;
