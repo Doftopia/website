8317d55932a86aed4fb19e8676ea1489c3b68378
flizze <alexandre.bel@hotmail.com>
2024-03-16 18:44:03 +0100

    added css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,38 +28,45 @@ const Home: React.FC = () => {
     };
 
     return (
-        <div>
+        <div className="bg-gray-800">
             <input
                 type="text"
                 value={nameFilter}
                 onChange={handleNameInputChange}
                 placeholder="Filter by item name"
             />
-            {items.map((item: any, index: number) => (
-              <div key={index} className="bg-black">                    
-              <img src={item.img} alt={item.name} />
-                    <h2>{item.name}</h2>
-                    <h3>{item.description}</h3>
-                    <h3>niveau {item.level}</h3>
-                    <h3>{item.type}</h3>
-                    {item.effects.map((effect: any, idx: number) => (
-                        effect.characteristic !== -1 && (
-                            <div key={idx}>
-                                <img src={effect.characImg} alt={effect.characName} />
-                                {effect.to ? (
-                                    <>
-                                        {effect.from} a {effect.to} {effect.characName}
-                                    </>
-                                ) : (
-                                    <>
-                                        {effect.from} {effect.characName}
-                                    </>
-                                )}
-                            </div>
-                        )
-                    ))}
-                </div>
-            ))}
+            <div className="grid grid-cols-4 mx-52 mt-3 gap-3">
+              {items.map((item: any, index: number) => (
+                <div id={index.toString()} className="bg-gray-900 text-white px-3 mb-7 pb-2 rounded-sm border-black border">  
+                <div className="flex  justify-between mt-4 pb-3 mb-4 border-white border-b">
+                  <div className="flex flex-col">
+                    <h2 className="font-bold">{item.name}</h2>
+                    <h3 className="text-   mb-5 text-gray-400">{item.type} - niveau {item.level}</h3>
+                  </div>
+                  <img src={item.img} alt={item.name} draggable='false' className="size-24 bg-gray-800 p-2 rounded-sm border border-black"/>
+                </div>                  
+                      {/* <h3 className="w-fit mb-3 text-sm">{item.description}</h3> */}
+                      {item.effects.map((effect: any, idx: number) => (
+                          effect.characteristic !== -1 && (
+                              <div key={idx} className="flex items-center">
+                                  <img src={effect.characImg} alt={effect.characName} className="mr-1 size-7" draggable='false'/>
+                                  <p className={effect.from < 0 || effect.to < 0 ? "text-red-500 text-sm" : "text-sm"}> 
+                                    {effect.to ? (
+                                        <>
+                                            {effect.from} a {effect.to} {effect.characName}
+                                        </>
+                                    ) : (
+                                        <>
+                                            {effect.from} {effect.characName}
+                                        </>
+                                    )}
+                                  </p>
+                              </div>
+                          )
+                      ))}
+                  </div>
+              ))}
+            </div>
         </div>
     );
 };
